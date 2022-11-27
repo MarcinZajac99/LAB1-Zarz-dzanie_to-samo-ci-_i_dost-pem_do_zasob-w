be86d67ffa76841f51e9a4a569c144ab7869ca6a
MarcinZajac99 <105553534+MarcinZajac99@users.noreply.github.com>
2022-11-27 15:47:30 +0100

    Update login.js

--- login_project/login.js
+++ login_project/login.js
@@ -1,15 +1,19 @@
+//pobieranie za pomoca id zmiennych do obiektow html
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+//dopisanie wywolania akcji do przycisku
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-
+//ustawienie prawidlowych danych
     if (username === "user1" && password === "12345") {
+//informacja o udanym zalogowaniu sie
         alert("You have successfully logged in.");
         location.reload();
+//informacja o podaniu niewlasciwych danych
     } else {
         alert("Wrong username or password.");
     }
